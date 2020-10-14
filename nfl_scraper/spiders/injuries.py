02e97d3cb5b7924d0d08743a707d7ae375c91876
RachelMcNeely <mcneelyra@g.cofc.edu>
2020-10-13 21:37:11 -0400

    updated spider to now work for all seasons and not just 2020

--- nfl_scraper/spiders/injuries.py
+++ nfl_scraper/spiders/injuries.py
@@ -31,39 +31,22 @@ class InjuriesSpider(scrapy.Spider):
         injury_lst = values[2::5]
         participation = values[3::5]
 
-        # try:
-        
-        
-        # # prev_year_str = response.css('.d3-o-dropdown option')[1].get()
-        # next_week_str = response.css('.nfl-c-form__group:nth-child(2) .d3-o-dropdown option')[1].get()
-        # # print('PREVIOUS YR STR: ', prev_year_str)
-        # print('NEXT WEEK STR: ', next_week_str)
-        # # prev_year = re.match('<option value="(.*)">', prev_year_str).groups()[0]
-        # next_week = re.match('<option value="(.*)"', next_week_str).groups()[0]
-        # print('** NEXT WEEK: ', next_week)
-        # # print('** PREVIOUS YEAR: ', prev_year)
-
-        # # if prev_year is not None:
-        # #     yield response.follow(prev_year, callback=self.parse)
-
         print()
         print('CURRENT URL: ', response.url)
         print()
 
-        current_year = re.match('https://www.nfl.com/injuries/league/(.*)/', response.url).groups()[0]
-        current_week = re.match('https://www.nfl.com/injuries/league/{}/(.*)'.format(current_year), response.url).groups()[0]
-        week_key = current_year + current_week 
+        current_week = re.match('https://www.nfl.com/injuries/league/{}/(.*)'.format(self.year), response.url).groups()[0]
+        week_key = self.year + current_week 
 
-        weeks = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "nfl-c-form__group", " " )) and (((count(preceding-sibling::*) + 1) = 2) and parent::*)]//*[contains(concat( " ", @class, " " ), concat( " ", "d3-o-dropdown", " " ))]/option/@value').re(r'/2020/REG(.*)')
+        ## TO DO:
+        ##   - This xpath only looks for REG season weeks - need to add in pre and post season as well?
 
-        print(weeks)
+        weeks = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "nfl-c-form__group", " " )) and (((count(preceding-sibling::*) + 1) = 2) and parent::*)]//*[contains(concat( " ", @class, " " ), concat( " ", "d3-o-dropdown", " " ))]/option/@value').re(r'/'+self.year+'/REG(.*)')
 
-        site_url = 'https://www.nfl.com/injuries/league/2020/REG{week}'
+        site_url = 'https://www.nfl.com/injuries/league/{year}/REG{week}'
+
         for week in weeks:
-            yield scrapy.Request(site_url.format(week=week), callback=self.parse)
-
-        # except:
-        #     print('Failed.')
+            yield scrapy.Request(site_url.format(year=self.year, week=week), callback=self.parse)
 
         for i, player in enumerate(players):
             engine = db_connect()
